4991292ad0876af8647440030c3da904f00ae3ba
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-05-01 23:31:21 +1000

    finished seting up the entire environment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Box and Dots</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p> Using Svelte and Tailwind! with Tauri</p>
