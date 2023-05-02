96088eaec0ac436436d639019199f29bc6fd1029
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-05-02 21:21:18 +1000

    some java script to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
 <center>
   
   <h1 class="mt-[15px] text-4xl font-bold text-center">Main Menu</h1>
@@ -14,8 +21,11 @@
 
 <center>
   
-  <button class="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[100px] rounded-full w-[600px] h-[200px] text-6xl font-bold text-center">
+  <button class="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[100px] rounded-full w-[600px] h-[200px] text-6xl font-bold text-center" on:click={increment}>
     Start Game!
   </button>
 </center>
 
+<center class="mt-[15px] text-2xl font-bold text-center">
+  <p>Clicks: {count}</p>
+</center>
