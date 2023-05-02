c523a82adc8e6c241bf94e50f9f780b98bb86fd3
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-05-02 17:42:32 +1000

    gui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
-<h1>Welcome to Box and Dots</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p> Using Svelte and Tailwind! with Tauri</p>
+<center>
+  
+  <h1 class="text-4xl font-bold text-center">Main Menu</h1>
+
+  <p>Box and Dots Beta</p>
+  <p> Using Svelte and Tailwind with Tauri </p>
+</center>
 
 <style lang="postcss">
     :global(html) {
       background-color: theme(colors.gray.400);
     }
   </style>
+
+<center>
+  
+  <button class="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[100px] rounded-full w-[600px] h-[200px] text-6xl font-bold text-center">
+    Start Game!
+  </button>
+</center>
 
