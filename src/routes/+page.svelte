cd50f79a9da9c663bd974a720750390b928073a1
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-05-02 16:16:39 +1000

    tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <h1>Welcome to Box and Dots</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p> Using Svelte and Tailwind! with Tauri</p>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.400);
+    }
+  </style>
+
