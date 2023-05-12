a8085723d33e537f463ede4aaf42a805d132a32e
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-05-12 21:05:31 +1000

    mute reactive, across routes+new music test sounds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,30 +4,51 @@ import { goto } from '$app/navigation';
 import { sound } from 'svelte-sound';
 import { onMount } from 'svelte';
 import "../app.css";
-import j$ from 'jquery';
 
+    // Check if the page has been reloaded or not
+    var reloaded = localStorage.getItem("reloaded");
+  
+    // If not, set the reloaded flag to true and reload the page
+    if (!reloaded) {
+      localStorage.setItem("reloaded", true);
+      location.reload(true);
+    }
+  
+    // If yes, clear the reloaded flag and proceed normally
+    else {
+      localStorage.removeItem("reloaded");
+      // Your normal code here
+    }
+  
 
 let clicked = false;
-let showPopup = false;
+  let showPopup = false;
 
-
-
- 
-
-
-  let audioFile: HTMLAudioElement = new Audio('https://audio.jukehost.co.uk/oLi9mXMDDx1Jb5whJjRPMzdTogQFcS82');
+  let audioFile: HTMLAudioElement = new Audio(
+    "https://audio.jukehost.co.uk/oLi9mXMDDx1Jb5whJjRPMzdTogQFcS82"
+  );
   audioFile.play();
-  audioFile.loop = true
+  audioFile.loop = true;
 
   function mute() {
-    clicked = !clicked;
+    clicked = !clicked; // toggle clicked
     if (audioFile.paused) {
       audioFile.play();
     } else {
       audioFile.pause();
     }
+    localStorage.setItem("clicked", String(clicked)); // store clicked in localStorage
     return true;
   }
+
+  onMount(() => {
+    // get clicked from localStorage on page load
+    clicked = localStorage.getItem("clicked") === "true";
+    // pause audio if clicked is true
+    if (clicked) {
+      audioFile.pause();
+    }
+  });
 
 
 
@@ -84,7 +105,7 @@ let showPopup = false;
   
     <h1 class="text-6xl mt-[0px] text-4xl drop-shadow-lg font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-300  to-blue-900 transform translate-y-[80px] font-minecraft">Dots and Boxes</h1>
 
-  <button class="ripple-bg-blue-600  g-clip-text bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-blue-800   text-white transform translate-y-[100px] font-minecraft py-2 px-4 mt-[00px] rounded-full w-[600px] h-[200px] text-[90px] font-bold text-center active:" on:click={() => setTimeout(() => goto('/game'), 300)}>
+  <button class="ripple-bg-blue-600  g-clip-text bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-blue-800   text-white transform translate-y-[100px] font-minecraft py-2 px-4 mt-[00px] rounded-full w-[600px] h-[200px] text-[90px] font-bold text-center active:" on:click={() => setTimeout(() => goto('/game'), 200)}>
     Play!
   </button>
 
@@ -97,15 +118,22 @@ let showPopup = false;
 </center>
 
 <center>
-    <button class="ripple-bg-gray-600  g-clip-text bg-gradient-to-r from-gray-600 to-gray-800 hover:bg-blue-800 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[120px]  rounded-full w-[250px] h-[100px] text-4xl font-minecraft text-center active:" on:click={() => setTimeout(() => goto('/settings'), 300)}> 
+    <button class="ripple-bg-gray-600  g-clip-text bg-gradient-to-r from-gray-600 to-gray-800 hover:bg-blue-800 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[120px]  rounded-full w-[250px] h-[100px] text-4xl font-minecraft text-center active:" on:click={() => setTimeout(() => goto('/settings'), 200)}> 
         Settings </button>
 </center>
 
-<button class="text-blue-600 hover:text-blue-900 font-minecraft font-bold text-center fixed bottom-2 right-2 " on:click={mute}>
-  <img src={clicked ? 'https://i.imgur.com/arTIVuh.png' : 'https://i.imgur.com/ljVuBNm.png'} alt="sound icon" width="30px" height="30px" />
+<button
+  class="text-blue-600 hover:text-blue-900 font-minecraft font-bold text-center fixed bottom-2 right-2 "
+  on:click={mute}
+>
+  <img
+    src={clicked ? "https://i.imgur.com/arTIVuh.png" : "https://i.imgur.com/ljVuBNm.png"}
+    alt="sound icon"
+    width="30px"
+    height="30px"
+  />
 </button>
 
 <p class="font-minecraft text-gray-300 fixed bottom-2 left-2">©The_Bi11iona1re</p>
 </div>
-
 
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts"> 
-  import audioFile from "/Users/aditya/Downloads/GitHub/Box-and-Dots--1/src/routes/+page.svelte";
+  import pop from "/Users/aditya/Downloads/GitHub/Box-and-Dots--1/src/routes/+page.svelte";
   import { goto } from '$app/navigation';
   import { sound } from 'svelte-sound';
   import { onMount } from 'svelte';
-
-
+  let buttonSound: HTMLAudioElement;
   let showModal = false
   let data = '';
 
@@ -26,14 +25,55 @@
 
 
 
+  let clicked: boolean;
   let gameAI: boolean; // declare a typescript variable
 
   // get the value of gameAI from local storage and parse it as a boolean
   gameAI = localStorage.getItem("gameAI") === "true";
 
+  clicked = localStorage.getItem("clicked") === "true"
+
+
+
+  clicked = !clicked;
+
+  let audioFile = new Audio(
+      "https://audio.jukehost.co.uk/GkT5w2H7t9KL3ZMUbucVG7qVHr50NjW2"
+    );
+    audioFile.loop = true; // set loop to true
+
+    // play or pause the audio file based on clicked
+    if (clicked === true) {
+      audioFile.play();
+    } else {
+      audioFile.pause();
+    }
+
+
+
+    let score = 0; // declare a variable for the score
+let scoreSound: HTMLAudioElement; // declare a variable for the sound effect
+
+
+
+
 </script>
+
+<audio
+  src="https://audio.jukehost.co.uk/1Do1qes84j02HAhuxaMy1WI8SJpd0jMX" 
+  preload="auto" 
+  bind:this={buttonSound} 
+></audio>
 
 <button class="ripple-bg-gray-600  g-clip-text bg-gradient-to-r from-gray-600 to-gray-800 hover:bg-blue-800 hover:bg-gray-800 text-white font-bold py-2 px-4 mt-[10px]   rounded-full w-[250px] h-[100px] text-4xl font-minecraft text-center active:" on:click={() => setTimeout(() => goto('/'), 200)}> 
   Main Menu </button>
 
-<p>Ai = {gameAI}</p>
+
+  <button on:click={() => {
+    if (clicked) { // check if clicked is true
+      buttonSound.volume = 0.4; // set the volume to 0.4
+      buttonSound.play(); // play the sound effect
+    }
+  }}>Click me to play a sound effect</button>
+
+<p>Ai = {gameAI} Sound = {clicked}</p>
