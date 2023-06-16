8daa9caeb8135b2ad5faf4ba0aab2472a37d5181
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2023-06-16 10:01:47 +1000

    run rust code from the frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,21 @@
   windows_subsystem = "windows"
 )]
 
+// Also in main.rs
 fn main() {
   tauri::Builder::default()
+    // This is where you pass in your commands
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("failed to run app");
+}
+
+
+
+
+
+#[tauri::command]
+fn my_custom_command() {
+  let x = 3;
+  println!("Main Menu!");
 }
